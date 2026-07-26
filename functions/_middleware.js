edb56a15fddcd1466f29331ad85b4f080896788d
Paul <enigma@enig.net>
2026-07-26 15:22:14 +0100

    Load verified base item selection handler

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -123,6 +123,7 @@ export async function onRequest(context) {
 })();
 </script>
 <script src="/data-source-fixes.js" defer></script>
+<script src="/base-item-selection.js" defer></script>
 <script src="/manifest.js" defer></script>
 <script src="/recipe-integrity.js" defer></script>
 <script src="/multi-review.js" defer></script>
